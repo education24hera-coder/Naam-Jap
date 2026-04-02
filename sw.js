5fd15875f65aabb4d13658da0ee0cffaab3de41d
education24hera-coder <education24hera@gmail.com>
2026-04-02 21:13:32 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'naam-jap-cache-v8';
+﻿const CACHE_NAME = 'naam-jap-cache-v9';
 const urlsToCache = [
   './index.html',
   './manifest.json',
@@ -61,6 +61,7 @@ self.addEventListener('activate', event => {
     })
   );
 });
+
 
 
 
